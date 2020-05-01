86b2320be9dd2a56b2b98315ef04e5493bc767e7
Laurent Kempé <laurent.kempe@techheadbrothers.com>
2020-05-01 20:15:39 +0200

    Add Radzen page

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Radzen;
 
 namespace BlazorApp
 {
@@ -18,6 +19,8 @@ namespace BlazorApp
             builder.RootComponents.Add<App>("app");
 
             builder.Services.AddTransient(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddScoped<DialogService>();
+            builder.Services.AddScoped<NotificationService>();
 
             await builder.Build().RunAsync();
         }
